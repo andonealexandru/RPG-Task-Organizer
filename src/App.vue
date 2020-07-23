ae801a2b2d156038e205891a598b384b5e6caccd
dragos0804 <dragospaul120@gmail.com>
2020-07-23 22:37:01 +0300

    colors changed

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,8 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background: #1d1223 no-repeat;
-  background-size: 100% auto;
-  overflow: auto;
-  min-height: 100vh;
+  background: rgb(86,103,113);
+  background: linear-gradient(90deg, rgba(86,103,113,1) 0%, rgba(218,196,148,1) 33%, rgba(86,103,113,1) 100%);min-height: 100vh;
   min-width: 100vw;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,38 @@
 <template>
   <div class="home">
-      <div class="actions-menu2 kong">
-        <div class="contain2">
-            <div class="col">
-                <ul>
-                    <li>
-                        <img src = "../assets/normal-pro.gif" class = "caracter">
-                    </li>
-                </ul>
-            </div>
-            <div class="col">
-                <h1 class="kong">Stats:</h1>
-                <ul>
-                  <li>Level: {{ heroLevel }}</li>
-                  <li>MAX HP: {{ maxHealth }}</li>
-                </ul>
-            </div>
-            <div class="col">
-                <h1 class="kong"><img src="../assets/coin.png" alt="gold" class="coin">: {{ userData.money }}</h1>
-
-            </div>
-            <div class = "col">
-                <ul>
-                    <li>
-                    </li>
-                    <li>
-                        <button type="button" class="nes-btn" :disabled="userData.money < 50" @click="breakTime">Break</button>
-                    </li>
-                    <li>
-                      <button type="button" class="nes-btn" @click="$router.push({name: 'Description'})">Home</button>
-                    </li>
-                </ul>
-            </div>
-            <div class = "col">
-              <ul>
-                <li>
-                </li>
-                <li>
-                  It costs you 50 <img src="../assets/coin.png" alt="gold"> to take a break... so start working
-                </li>
-              </ul>
-            </div>
+    <!-- Off Canvas -->
+    <vk-offcanvas-content>
+      <vk-button class="uk-margin-right-small button2" @click="slide = true">
+        <span class="dot">
+          <vk-icon icon="menu" style="color: #dfdde0;"></vk-icon>
+        </span>
+        
+      </vk-button>
+      <vk-offcanvas overlay mode="slide" :show.sync="slide">
+        <vk-offcanvas-close @click="slide = false"></vk-offcanvas-close>
+            <h1 style = "color: #dac494">Menu</h1>
+            <hr class="new1">
+            
+              <vk-button :disabled="userData.money < 50" @click="breakTime">Break</vk-button>
+                &nbsp;
+              <vk-button @click="$router.push({name: 'Description'})" style = "float: right;">Home</vk-button>
+              
+              <br>
+              <div style="position: absolute; bottom: 0;">  
+              <hr class="new1">
+              <p>
+                Current <img src="../assets/coin.png" alt="gold" style="width: 18px; height: 18px">: {{ userData.money }}
+              </p>
+              <p>
+                 It costs you 50 <img src="../assets/coin.png" alt="gold" style="width: 18px; height: 18px"> to take a break.
+              </p>
+              </div>
+        </vk-offcanvas>
+      </vk-offcanvas-content>
+      <!-- End of Off Canvas -->
 
 
-            <div class="clearfix" />
-            </div>
-    </div>
+    <!-- Grid -->
     <vk-grid class="uk-child-width-1-3@m uk-padding-large">
       <List
         group="todo"
@@ -66,6 +53,7 @@
         :updateUser="getUser"
       />
     </vk-grid>
+    <!-- End of grid -->
   </div>
 </template>
 
@@ -96,7 +84,8 @@
                   email: null,
                   level: null,
                   money: null
-              }
+              },
+          slide: false
           };
       },
       mounted() {
@@ -167,24 +156,9 @@
 </script>
 
 <style>
-  .nes-btn{
-        width: 100px;
-        cursor: pointer;
+hr.new1 {
+  border-top: 1px solid #113448;
 }
-  .caracter{
-        width: 430px;
-        height: 150px;
-        image-rendering: pixelated;
-        image-rendering: -moz-crisp-edges;
-        image-rendering: crisp-edges;
-  }
-  .coin{
-        width: 70px;
-        height: 70px;
-        image-rendering: pixelated;
-        image-rendering: -moz-crisp-edges;
-        image-rendering: crisp-edges;
-  }
   .flip-list-move {
     transition: transform 0.5s;
   }
@@ -204,107 +178,21 @@
   .list-group-item i {
     cursor: pointer;
   }
-  .actions-menu2 {
-    width: 100vw;
-    margin-left: 0;
-    top: 0;
-    height: auto;
-    background-color: #3c234a;
 
-  }
-
-  .actions-menu2 .col {
-    width: 190px;
-    height: auto;
-    float: left;
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    padding: 0px 20px 20px 20px;
-  }
-
-  .actions-menu2 .col h1 {
-    margin: 0;
-    padding: 0;
-
-    font-size: 15px;
-    line-height: 17px;
-    padding: 20px 0px 5px 0px;
-    color: rgba(255, 255, 255, 0.596);
-    font-weight: normal;
-    text-transform: uppercase;
-    letter-spacing: 0.250em;
-  }
-
-  .actions-menu2 .col ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-  }
-
-  .actions-menu2 .col ul li {
-    color: #999999;
-    font-size: 14px;
-
-    font-weight: bold;
-    padding: 5px 0px 5px 0px;
-    cursor: pointer;
-    transition: .2s;
-    -webkit-transition: .2s;
-    -moz-transition: .2s;
-  }
-
-  .actions-menu2 .col ul li:hover {
-    color: #ffffff;
-    transition: .1s;
-    -webkit-transition: .1s;
-    -moz-transition: .1s;
-  }
   .clearfix {
     clear: both;
   }
-
-@media only screen and (min-width: 1280px) {
-  .contain {
-    width: 1200px;
-    margin: 0 auto;
-  }
+.dot
+{
+  height: 36px;
+  width: 36px;
+  background-color: #113448;
+  border-radius: 50%;
+  position:absolute;
 }
-@media only screen and (max-width: 1139px) {
-  .contain .social {
-    width: 1000px;
-    display: block;
-  }
-  .social h1 {
-    margin: 0px;
-  }
-}
-@media only screen and (max-width: 950px) {
-  .actions-menu .col {
-    width: 33%;
-  }
-  .actions-menu .col h1 {
-    font-size: 14px;
-  }
-  .actions-menu .col ul li {
-    font-size: 13px;
-  }
-}
-@media only screen and (max-width: 500px) {
-    .actions-menu .col {
-      width: 50%;
-    }
-    .actions-menu .col h1 {
-      font-size: 14px;
-    }
-    .actions-menu .col ul li {
-      font-size: 13px;
-
-    }
-}
-@media only screen and (max-width: 340px) {
-  .actions-menu .col {
-    width: 100%;
-  }
+.button2{
+    background: transparent;
+    border: none !important;
+    font-size:0;
 }
 </style>
