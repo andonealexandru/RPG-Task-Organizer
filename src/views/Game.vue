e23339ebb95f8c4d2b14f526c850584139178bcb
Alexandru Andone <andonealexandru19@yahoo.com>
2020-07-28 14:46:12 +0300

    Solved css bug in game

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -3,12 +3,12 @@
 
     <div class="computer-monitor">
 
-    <unity src="js/Build/RPG%20Task%20Organizer.json" width="990" heighht="600"
-          unityLoader="js/Build/UnityLoader.js" />
+      <unity src="js/Build/RPG%20Task%20Organizer.json" width="990" heighht="600"
+             unityLoader="js/Build/UnityLoader.js" />
     </div>
     <div class="computer-bottom"></div>
-		<div class="computer-stand"></div>
-		<div class="computer-stand-bottom"></div>
+    <div class="computer-stand"></div>
+    <div class="computer-stand-bottom"></div>
 
   </div>
 </template>
@@ -23,8 +23,8 @@
     }
 </script>
 
-<style >
-  /*@import "https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css";*/
-  /*@import "../assets/game/style.css";*/
+<style>
+  @import "https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css";
+  @import "../assets/game/style.css";
   @import "../assets/game/TemplateData/style.css";
 </style>
